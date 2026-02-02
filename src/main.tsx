ff3bfe48a941bdfd0eab1b55158d3aeb19ee46d5
Ross Sinclair <rosssinclair83@gmail.com>
2026-02-02 16:37:56 +0000

    Add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -40,8 +40,11 @@ const router = createBrowserRouter([
   },
 ]);
 
+import { Analytics } from '@vercel/analytics/react';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <Analytics />
   </React.StrictMode>,
 );
